2990faaf0a4b638a2cc8a2abbfacef30c67d53d8
trietdang <trietdang@cca.edu>
2017-04-26 22:23:32 -0700

    change

--- identityhacking/drag/assets/js/script.js
+++ identityhacking/drag/assets/js/script.js
@@ -5,7 +5,7 @@ var video = document.querySelector("#videoElement");
 navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia || navigator.oGetUserMedia;
  
 if (navigator.getUserMedia) {       
-    navigator.getUserMedia({video: false}, handleVideo, videoError);
+    navigator.getUserMedia({video: true}, handleVideo, videoError);
 }
  
 function handleVideo(stream) {
